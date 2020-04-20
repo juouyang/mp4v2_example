ccf72bdbd985aa60dfcd87d12e1eed797dc5bc54
Ju Ouyang <juouyang@gmail.com>
2020-04-20 16:03:44 +0800

    Change to multi-thread

--- main.c
+++ main.c
@@ -16,6 +16,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <time.h>
+#include <pthread.h>
 #include <arpa/inet.h>
 
 #include <mp4v2/mp4v2.h>
@@ -37,14 +38,80 @@ static inline int _read_h264(FILE* m_fp_h264, unsigned char* buf, int buf_size);
 static inline int _read_one_nalu_from_buf(const unsigned char* buffer, unsigned int nBufferSize, unsigned int offSet, MP4ENC_NaluUnit* nalu);
 static inline int _write_h264(MP4TrackId* m_videoId, int* sps_wt, int* pps_wt, MP4FileHandle hMp4File, int w, int h, const unsigned char* pData, int size);
 
+static int running = 3;
+
+static void* thread_libfbm_video_0(void* arg) {
+    printf("thread_libfbm_video_0\n");
+    do {
+        int ret = _muxer_mp4("/media/sf_Downloads/foo/foo.h264"
+                , 1920
+                , 1080
+                , "/media/sf_Downloads/foo/foo.aac"
+                , 48000
+                , "/media/sf_Downloads/foo/foo.mp4");
+        printf("ret = %d\n", ret);
+    } while (0);
+    running--;
+    pthread_exit(NULL);
+}
+
+static void* thread_libfbm_video_1(void* arg) {
+    printf("thread_libfbm_video_1\n");
+    do {
+        int ret = _muxer_mp4("/media/sf_Downloads/foo/foo-1.h264"
+                , 1920
+                , 1080
+                , "/media/sf_Downloads/foo/foo-1.aac"
+                , 48000
+                , "/media/sf_Downloads/foo/foo-1.mp4");
+        printf("ret = %d\n", ret);
+    } while (0);
+    running--;
+    pthread_exit(NULL);
+}
+
+static void* thread_libfbm_video_2(void* arg) {
+    printf("thread_libfbm_video_2\n");
+    do {
+        int ret = _muxer_mp4("/media/sf_Downloads/foo/foo-2.h264"
+                , 1920
+                , 1080
+                , "/media/sf_Downloads/foo/foo-2.aac"
+                , 48000
+                , "/media/sf_Downloads/foo/foo-2.mp4");
+        printf("ret = %d\n", ret);
+    } while (0);
+    running--;
+    pthread_exit(NULL);
+}
+
 int main(int argc, char** argv) {
-    int ret = _muxer_mp4("/media/sf_Downloads/foo/foo.h264"
-            , 1920
-            , 1080
-            , "/media/sf_Downloads/foo/foo.aac"
-            , 16000
-            , "/media/sf_Downloads/foo/foo.mp4");
-    return ret;
+    int ret = 0;
+    pthread_t ThreadVideoFrameData_ID_1, ThreadVideoFrameData_ID_2, ThreadVideoFrameData_ID_3;
+
+    if ((ret = pthread_create(&ThreadVideoFrameData_ID_1, NULL, &thread_libfbm_video_0, (void *) NULL))) {
+        printf("Failed to create libfbm video thread 0, ret=[%d]", ret);
+        return -1;
+    }
+    pthread_detach(ThreadVideoFrameData_ID_1);
+
+    if ((ret = pthread_create(&ThreadVideoFrameData_ID_2, NULL, &thread_libfbm_video_1, (void *) NULL))) {
+        printf("Failed to create libfbm video thread 1, ret=[%d]", ret);
+        return -1;
+    }
+    pthread_detach(ThreadVideoFrameData_ID_2);
+
+    if ((ret = pthread_create(&ThreadVideoFrameData_ID_3, NULL, &thread_libfbm_video_2, (void *) NULL))) {
+        printf("Failed to create libfbm video thread 2, ret=[%d]", ret);
+        return -1;
+    }
+    pthread_detach(ThreadVideoFrameData_ID_3);
+
+    while (running > 0) {
+        sleep(1);
+    }
+
+    return 0;
 }
 
 static inline int _muxer_mp4(const char* h264, int width, int height, const char* aac, int sr, const char* mp4) {
